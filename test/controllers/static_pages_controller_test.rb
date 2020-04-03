b8ddc0622a0050bab1dbad05c71e4d84cb617996
annebeth <annebethbuis@gmail.com>
2020-04-03 20:14:57 +0100

    Add static pages controller and views.

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "NLP4DH"
+  end
+
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get documentation" do
+    get static_pages_documentation_url
+    assert_response :success
+    assert_select "title", "Documentation | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+end
