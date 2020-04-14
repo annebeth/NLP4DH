11fc9339e0c58bd6e224d52d8ded8ecee2e13b0f
annebeth <annebethbuis@gmail.com>
2020-04-14 15:31:24 +0100

    Make text_documents#index the root page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'text_documents#index'
   get '/documentation', to: 'static_pages#documentation'
   get '/about', to: 'static_pages#about'
   resources :text_documents
